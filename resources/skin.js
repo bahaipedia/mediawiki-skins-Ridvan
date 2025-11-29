998ae20c5a33e42313bfd58a24daf706ce0037ea
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2025-11-28 20:10:23 -0800

    Update skin.js

--- resources/skin.js
+++ resources/skin.js
@@ -4,7 +4,7 @@
  */
 ( function ( $ ) {
     $( function () {
-
+        console.log( 'Ridvan: skin.js loaded successfully.' );
         /* ==================================================================
            MENU LOGIC (Close on click-away, Exclusive opening)
            ================================================================== */
