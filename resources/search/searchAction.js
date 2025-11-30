34ea07c394e0cc2ec1984693eaa8fd604e39ab71
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2025-11-30 07:20:30 -0800

    Update searchAction.js

--- resources/search/searchAction.js
+++ resources/search/searchAction.js
@@ -1,14 +1,53 @@
-// We don't need to import config or urlGenerator anymore since we aren't generating actions
+const config = require( './config.json' );
+const urlGenerator = require( './urlGenerator.js' );
+
 function searchAction() {
 	return {
-		// EMPTY RENDER FUNCTION
+		urlGeneratorInstance: urlGenerator( config ),
+		
 		render: function ( searchQuery, templates ) {
-			// Ensure the action group stays hidden
+			const items = [];
+
+			// 1. "Search for pages containing..." link
+			items.push( {
+				id: 'fulltext',
+				// Generate URL: Special:Search?search=query&fulltext=1
+				href: this.urlGeneratorInstance.generateUrl( 'Special:Search', {
+					fulltext: '1',
+					search: searchQuery.value
+				} ),
+				icon: 'articleSearch',
+				text: mw.message( 'citizen-search-fulltext', searchQuery.value ).text()
+			} );
+
+			// 2. (Optional) "Edit/Create page" link - You can remove this block if you don't want it
+			items.push( {
+				id: 'editpage',
+				href: this.urlGeneratorInstance.generateUrl( searchQuery.value, { action: 'edit' } ),
+				icon: 'edit',
+				text: mw.message( 'citizen-search-editpage', searchQuery.value ).text()
+			} );
+
+			const data = {
+				type: 'action',
+				'array-list-items': items
+			};
+
+			const partials = {
+				TypeaheadListItem: templates.TypeaheadListItem
+			};
+
+			// Inject the HTML
+			const actionList = document.getElementById( 'citizen-typeahead-list-action' );
 			const actionGroup = document.getElementById( 'citizen-typeahead-group-action' );
-			if ( actionGroup ) {
-				actionGroup.hidden = true;
+			
+			if ( actionList && actionGroup ) {
+				actionList.outerHTML = templates.TypeaheadList.render( data, partials ).html();
+				// Show the group
+				document.getElementById( 'citizen-typeahead-group-action' ).hidden = false;
 			}
 		},
+		
 		clear: function () {
 			const actionList = document.getElementById( 'citizen-typeahead-list-action' );
 			if ( actionList ) actionList.innerHTML = '';
@@ -16,8 +55,9 @@ function searchAction() {
 			const actionGroup = document.getElementById( 'citizen-typeahead-group-action' );
 			if ( actionGroup ) actionGroup.hidden = true;
 		},
+		
 		init: function () {
-			// No initialization needed
+			// No init needed
 		}
 	};
 }
