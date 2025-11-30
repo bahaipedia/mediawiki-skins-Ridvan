44958e26263234d48efe03b28af452f5281aad80
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2025-11-29 22:29:16 -0800

    Update searchAction.js

--- resources/search/searchAction.js
+++ resources/search/searchAction.js
@@ -1,89 +1,25 @@
-const config = require( './config.json' );
-const urlGenerator = require( './urlGenerator.js' );
-
-const fulltextParam = {
-	fulltext: '1'
-};
-const mediasearchParam = {
-	type: 'image'
-};
-const editpageParam = {
-	action: 'edit'
-};
-
+// We don't need to import config or urlGenerator anymore since we aren't generating actions
 function searchAction() {
 	return {
-		urlGeneratorInstance: urlGenerator( config ),
-		// Assume user can't edit by default
-		userCanEdit: false,
-		userRights: undefined,
+		// EMPTY RENDER FUNCTION
 		render: function ( searchQuery, templates ) {
-			const items = [];
-
-			// TODO: Save this in a separate JSON file
-			// Fulltext search
-			fulltextParam.search = searchQuery.value;
-			items.push( {
-				id: 'fulltext',
-				href: this.urlGeneratorInstance.generateUrl( 'Special:Search', fulltextParam ),
-				icon: 'articleSearch',
-				text: mw.message( config.isAdvancedSearchExtensionEnabled ?
-					'citizen-search-advancedsearch' :
-					'citizen-search-fulltext'
-				)
-			} );
-
-			// MediaSearch
-			if ( config.isMediaSearchExtensionEnabled ) {
-				mediasearchParam.search = searchQuery.value;
-				items.push( {
-					id: 'mediasearch',
-					href: this.urlGeneratorInstance.generateUrl( 'Special:MediaSearch', mediasearchParam ),
-					icon: 'imageGallery',
-					text: mw.message( 'citizen-search-mediasearch' )
-				} );
+			// Ensure the action group stays hidden
+			const actionGroup = document.getElementById( 'citizen-typeahead-group-action' );
+			if ( actionGroup ) {
+				actionGroup.hidden = true;
 			}
-
-			/*
-			For some reason title.exists() always returns null
-			const title = mw.Title.newFromUserInput( searchQuery.value );
-			console.log( title.exists() );
-			*/
-
-			if ( this.userCanEdit ) {
-				// Edit/create page
-				// TODO: Check whether the page exists
-				items.push( {
-					id: 'editpage',
-					href: this.urlGeneratorInstance.generateUrl( searchQuery.value, editpageParam ),
-					icon: 'edit',
-					text: mw.message( 'citizen-search-editpage' )
-				} );
-			}
-
-			const data = {
-				type: 'action',
-				'array-list-items': items
-			};
-
-			const partials = {
-				TypeaheadListItem: templates.TypeaheadListItem
-			};
-
-			document.getElementById( 'citizen-typeahead-list-action' ).outerHTML = templates.TypeaheadList.render( data, partials ).html();
-			document.getElementById( 'citizen-typeahead-group-action' ).hidden = false;
 		},
 		clear: function () {
-			document.getElementById( 'citizen-typeahead-list-action' ).innerHTML = '';
-			document.getElementById( 'citizen-typeahead-group-action' ).hidden = true;
+			const actionList = document.getElementById( 'citizen-typeahead-list-action' );
+			if ( actionList ) actionList.innerHTML = '';
+
+			const actionGroup = document.getElementById( 'citizen-typeahead-group-action' );
+			if ( actionGroup ) actionGroup.hidden = true;
 		},
 		init: function () {
-			mw.user.getRights().done( ( rights ) => {
-				this.userCanEdit = rights.includes( 'createpage', 'edit' );
-			} );
+			// No initialization needed
 		}
 	};
 }
 
-/** @module searchAction */
 module.exports = searchAction;
