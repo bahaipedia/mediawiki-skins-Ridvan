bab54e0faa82172065c3effa2182896322efb081
Sarah Haslip <103548776+shaslip@users.noreply.github.com>
2026-07-04 09:16:34 -0700

    Update Ridvan.php

--- includes/Ridvan.php
+++ includes/Ridvan.php
@@ -65,8 +65,12 @@ class SkinRidvan extends SkinMustache {
         // ---------------------------------------------------------
         // PART 2: HEADER BUTTONS (ACTIONS)
         // ---------------------------------------------------------
+        $namespaces = $data['data-portlets']['data-associated-pages']['array-items'] 
+            ?? $data['data-portlets']['data-namespaces']['array-items'] 
+            ?? [];
+
         $allPortlets = array_merge(
-            $data['data-portlets']['data-namespaces']['array-items'] ?? [],
+            $namespaces,
             $data['data-portlets']['data-views']['array-items'] ?? [],
             $data['data-portlets']['data-actions']['array-items'] ?? []
         );
